b2bc9087f883eb2e62730103801b8d34881f8a0d
Iptysam <mukadamiptisam@gmail.com>
2022-10-12 12:50:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+
+
+
+
+const form = document.querySelector('.score_form');
+const refreshBtn = document.querySelector('.refresh-button');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  postData();
+  nameInput.value = '';
+  scoreInput.value = '';
+});
+
+refreshBtn.addEventListener('click', () => {
+  renderScore();
+  window.location.reload();
+});
+
+window.onload = renderScore();
